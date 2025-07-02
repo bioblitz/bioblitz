b01fe0093c13d06c98690505d9e7192ba0271ab2
Aarnav Suwal <aarnavsuwal@Aarnavs-MacBook-Air-5.local>
2025-07-02 14:34:29 -0400

    added a main page

---
+++ bioblitz/src/app/page.tsx
@@ -0,0 +1,15 @@
+export default function AboutPage() {
+  return (
+    <html>
+        <body>
+ <div>
+      <h1>bioblitz</h1>
+      <p>#put something here idk#</p>
+    </div>
+
+    </body>
+
+    </html>
+   
+  );
+}
