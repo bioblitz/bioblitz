26b2b2301412addadef593ba858363ca7294c7b8
Aarnav Suwal <aarnavsuwal@Aarnavs-MacBook-Air-5.local>
2025-07-07 13:16:45 -0400

    added an authentication page, fixed some of the root layout stuff, added googleoauthprovider to our website

---
+++ bioblitz/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import '@/styles/globals.css' // Make sure this line is here
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}
+>
+<html lang="en">
+      <body>
+        {
+        /*We will put the navbar here when deepak is finished with making it*/
+        }
+        {children}
+      </body>
+    </html>
+    </GoogleOAuthProvider>
+    
+  );
+}
