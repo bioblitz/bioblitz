7f4349a5a4b939a6097ea250e08d355818cb395b
Aarnav Suwal <aarnavsuwal@Aarnavs-MacBook-Air-5.local>
2025-11-14 15:46:29 -0500

    temporary push

--- bioblitz/src/components/layout/UserNav.tsx
+++ bioblitz/src/components/layout/UserNav.tsx
@@ -48,20 +48,39 @@ const UserNav = () => {
 
   return (
     <div className="relative" ref={dropdownRef}>
-      <div onClick={() => setDropdownOpen(!dropdownOpen)} className="flex items-center space-x-4 cursor-pointer">
-        <div className="relative">
-          {user.photoURL ? (
-            <img
-              src={user.photoURL}
-              alt={user.displayName}
-              className="h-11 w-11 rounded-full"
+      <div className="flex items-center space-x-2">
+          <div className="relative">
+            {user.photoURL ? (
+              <img
+                src={user.photoURL}
+                alt={user.displayName}
+                className="h-11 w-11 rounded-full"
+              />
+            ) : (
+              <DefaultAvatar name={user.displayName} />
+            )}
+          </div>
+
+        <button
+          onClick={() => setDropdownOpen(!dropdownOpen)}
+          className="p-1 rounded-full text-zinc-400 hover:text-white hover:bg-zinc-700/50"
+          aria-label="Open user menu"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="w-5 h-5"
+          >
+            <path
+              fillRule="evenodd"
+              d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z"
+              clipRule="evenodd"
             />
-          ) : (
-            <DefaultAvatar name={user.displayName} />
-          )}
-        </div>
-        <span className="text-white">{user.displayName}</span>
+          </svg>
+        </button>
       </div>
+
       {dropdownOpen && (
         <div className="absolute right-0 mt-2 w-40 bg-zinc-900 rounded-lg shadow-lg overflow-hidden z-50">
           <Link href="/profile">
